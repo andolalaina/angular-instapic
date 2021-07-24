0010e7bc4a1de9ca74893d4df6fd271fb46b85a5
Harry Michael <ando.lalaina1103@gmail.com>
2021-07-24 14:44:22 +0300

    Added stories and posts components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StoriesComponent } from './components/stories/stories.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { StoryComponent } from './components/story/story.component';
+import { PostComponent } from './components/post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StoriesComponent,
+    PostsComponent,
+    StoryComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule
