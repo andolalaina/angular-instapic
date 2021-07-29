3b28fc0734a86161137cdac5d1643a3a5823c4a0
Harry Michael <ando.lalaina1103@gmail.com>
2021-07-29 14:00:28 +0300

    Added user informations

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -4,6 +4,7 @@ import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
 
 import { PostService } from 'src/app/services/post.service';
 import { Media } from 'src/app/media';
+import { User } from 'src/app/user';
 
 @Component({
   selector: 'app-post',
@@ -11,9 +12,9 @@ import { Media } from 'src/app/media';
   styleUrls: ['./post.component.css'],
 })
 export class PostComponent implements OnInit, AfterViewInit {
-  @Input() user!: object;
+  @Input() user!: User;
   @Input() description!: string;
-  @Input() likeNumber!: number;
+  @Input() likeNumber = Math.floor(Math.random()*1000);
   @Input() uploadDate!: string;
   @Input() medias : Media[] = [];
 
@@ -43,6 +44,9 @@ export class PostComponent implements OnInit, AfterViewInit {
         this.medias.push(media)
       })
     }
+    this.postService.getUser().subscribe((fecthedUser) => {
+      this.user = fecthedUser
+    })
   }
 
   ngAfterViewInit(): void {
--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -1,17 +1,24 @@
 import { Injectable } from '@angular/core';
 import { Observable, observable, of } from 'rxjs';
 import { Media } from '../media';
+import { User } from '../user';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   getMedias(): Observable<Media> {
     const standardMedia:Media = {
       url: "https://picsum.photos/"+Math.floor(Math.random()*1000)
     }
     return of(standardMedia)
+  }
+
+  getUser(): Observable<User> {
+    const userApi = "https://randomuser.me/api/"
+    return this.http.get<User>(userApi)
   }
 }
